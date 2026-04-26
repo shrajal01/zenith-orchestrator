c9d85cf46a7b7471838f73796865da6836356e7e
shrajal01 <sp751430@gmail.com>
2026-04-27 01:08:07 +0530

    final cors error

--- app/main.py
+++ app/main.py
@@ -20,8 +20,8 @@ app = FastAPI(title="Star Orchestrator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.netlify\.app",  
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
