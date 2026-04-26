bc8beecb2875a353720a1f5ea37836a2975c5af9
shrajal01 <sp751430@gmail.com>
2026-04-27 02:04:42 +0530

    fix db connection for render

--- app/core/database.py
+++ app/core/database.py
@@ -10,7 +10,7 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
